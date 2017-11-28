074bf3f47381b7797e6c3e5463a359e82de83139
nlister <clister4@gmail.com>
2017-11-28 13:57:43 -0800

    Try with recursion and object dependencies.

--- TPGTest/Program.cs
+++ TPGTest/Program.cs
@@ -12,14 +12,17 @@ namespace TPGTest
     {
         static void Main()
         {
-            string[] args = { "FileProcessor: ProcessingLibrary", "ProcessingLibrary: " };
+            //string[] args = { "SupremeLeaderSnoke: ", "Resistance: LeiaOrgana", "LeiaOrgana: AnakinSkywalker",
+            //"FirstOrder: SupremeLeaderSnoke", "StarWars: Resistance",  "AnakinSkywalker: "};
+            //string[] args = {"SupremeLeaderSnoke: ", "Resistance: LeiaOrgana", "LeiaOrgana: AnakinSkywalker",
+            //"FirstOrder: SupremeLeaderSnoke", "StarWars: ", "AnakinSkywalker: Resistance", "Resistance: AnakinSkywalker"};
+            string[] args = { "FileProcessor: ProcessingLibrary", "ProcessingLibrary: FileProcessor" };
 
             var output = GatherDependencies(args);
-       
 
-            foreach (var item in output)
+            foreach (var l in OrderDependencies(output, x => x.Dependencies, new PackageComparer()))
             {
-                Console.WriteLine(item.Name);
+                Console.WriteLine(l.Name);
             }
         }
 
@@ -32,37 +35,32 @@ namespace TPGTest
                 var item = new Tuple<string, string>(dependencies[i].Split(':')[0], dependencies[i].Split(':')[1].TrimStart(' '));
                 retVal.Add(item.Item2.Length > 0 ? new Package(item.Item1, new Package(item.Item2)) : new Package(item.Item1));
             }
-
+            
             return retVal.OrderBy(x => x.Dependencies.Count()).ToList();
         }
 
-        private static List<string> OrderDependencies(List<Tuple<string, string>> dependencies)
+        public static List<T> OrderDependencies<T>(ICollection<T> source, Func<T, IEnumerable<T>> getDependencies, IEqualityComparer<T> comparer = null)
         {
-            var retVal = new List<string>();
-
-            foreach (var d in dependencies)
-            {
-                if (d.Item2 == string.Empty || d.Item2.Length == 0)
+            var visiteds = new Dictionary<T, bool>(comparer);
+            var sorted = new List<T>(source.Count);
+            Action<T> visit = null;
+            visit = item => {
+                if (visiteds.TryGetValue(item, out bool isMarked))
                 {
-                    retVal.Add(d.Item1);
+                    if (isMarked) return;
+                    throw new ArgumentException("Dependency specification is invalid, it contains cycles.");
                 }
-            }
-
-            foreach (var d in dependencies)
-            {
-                if (d.Item2.Length > 0 && !retVal.Contains(d.Item2))
+                var dependencies = getDependencies(item);
+                if (dependencies != null)
                 {
-                    retVal.Add(d.Item2);
+                    visiteds[item] = false;
+                    foreach (var itm in dependencies) visit(itm);
                 }
-            }
-
-            foreach (var d in dependencies)
-            {
-                if (!retVal.Contains(d.Item1))
-                    retVal.Add(d.Item1);
-            }
-
-            return retVal;
+                visiteds[item] = true;
+                sorted.Add(item);
+            };
+            foreach (var item in source) visit(item);
+            return sorted;
         }
 
         public class Package
@@ -74,6 +72,19 @@ namespace TPGTest
             {
                 Name = name;
                 Dependencies = dependencies;
+            }
+        }
+
+        public class PackageComparer : EqualityComparer<Package>
+        {
+            public override bool Equals(Package x, Package y)
+            {
+                return (x == null && y == null) || (x != null && y != null && x.Name == y.Name);
+            }
+
+            public override int GetHashCode(Package obj)
+            {
+                return obj == null ? 0 : obj.Name.GetHashCode();
             }
         }
 
